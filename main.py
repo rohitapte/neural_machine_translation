5e58390099e558b8716f55f480f19867484f7979
Rohit Apte <rohit@rohitapte.com>
2019-04-22 12:01:46 +0800

    added save and load model

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import io
 import json
 import os
 from utils.data_utils import build_tokenizer_and_split_text
-from tensorflow.python.keras.utils import to_categorical
 from utils.data_generator import DataGenerator
-from models.encoder_decoder_lstm import define_nmt
+from tensorflow.python.keras.models import load_model
+
+WHICH_MODEL="LSTM"
+saveParams={}
+if WHICH_MODEL=='LSTM':
+    from models.encoder_decoder_lstm import define_nmt
+
+def save_model(dir_hash,full_model,encoder_model,decoder_model,source_tokenizer,target_tokenizer):
+    tokenizer_json=source_tokenizer.to_json()
+    with io.open('h5.models/'+dir_hash+'/source_tokenizer.json','w',encoding='utf-8') as f:
+        f.write(json.dumps(tokenizer_json,ensure_ascii=False))
+    tokenizer_json=target_tokenizer.to_json()
+    with io.open('h5.models/'+dir_hash+'/target_tokenizer.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(tokenizer_json, ensure_ascii=False))
+    full_model.save('h5.models/'+dir_hash+'/full_model.h5')
+    encoder_model.save('h5.models/'+dir_hash+'/encoder_model.h5')
+    decoder_model.save('h5.models/'+dir_hash+'/decoder_model.h5')
 
 if __name__ == '__main__':
     #Define model parameters
@@ -12,24 +27,13 @@ if __name__ == '__main__':
 
     SOURCE_TIMESTEPS,TARGET_TIMESTEPS=100,100
     HIDDEN_SIZE=128
-    EMBEDDING_DIM=100
+    EMBEDDING_DIM=101
     NUM_EPOCHS=10
     BATCH_SIZE=32
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID";
     # The GPU id to use, usually either "0" or "1";
     os.environ["CUDA_VISIBLE_DEVICES"] = WHICH_GPU;
-
-    def save_model(model,source_tokenizer,target_tokenizer):
-        if not os.path.exists(os.path.join('..', 'h5.models')):
-            os.mkdir(os.path.join('..', 'h5.models'))
-        tokenizer_json=source_tokenizer.to_json()
-        with io.open('h5.models/source_tokenizer.json','w',encoding='utf-8') as f:
-            f.write(json.dumps(tokenizer_json,ensure_ascii=False))
-        tokenizer_json=target_tokenizer.to_json()
-        with io.open('h5.models/target_tokenizer.json', 'w', encoding='utf-8') as f:
-            f.write(json.dumps(tokenizer_json, ensure_ascii=False))
-        model.save('h5.models/model.h5')
 
     src_train,src_test,tgt_train,tgt_test,source_tokenizer,target_tokenizer=build_tokenizer_and_split_text()
     if source_tokenizer.num_words is None:
@@ -42,14 +46,38 @@ if __name__ == '__main__':
         tgt_vsize=target_tokenizer.num_words
     print('Source Vocab {}'.format(src_vsize))
     print('Target Vocab {}'.format(tgt_vsize))
+
+    model_params = WHICH_MODEL + "_" + str(HIDDEN_SIZE) + "_" + str(EMBEDDING_DIM) + "_" + str(
+        SOURCE_TIMESTEPS) + "_" + str(TARGET_TIMESTEPS)+"_"+str(src_vsize)+"_"+str(tgt_vsize)
+    dir_hash = model_params
+
+    if not os.path.exists('h5.models/' + dir_hash):
+        os.makedirs('h5.models/' + dir_hash)
+    model_dict = {
+        'Model': WHICH_MODEL,
+        'HiddenSize': HIDDEN_SIZE,
+        'EmbeddingDim': EMBEDDING_DIM,
+        'SourceTimeSteps': SOURCE_TIMESTEPS,
+        'TargetTimeSteps': TARGET_TIMESTEPS,
+        'SourceVocab':src_vsize,
+        'TargetVocab':tgt_vsize,
+    }
+    with open('h5.models/' + dir_hash + "/model_params.json",'w') as f:
+        f.write(json.dumps(model_dict))
+
     training_generator=DataGenerator(source_text=src_train,target_text=tgt_train,source_tokenizer=source_tokenizer,target_tokenizer=target_tokenizer,
                                      target_vocab_size=tgt_vsize,source_timesteps=SOURCE_TIMESTEPS,target_timesteps=TARGET_TIMESTEPS,batch_size=BATCH_SIZE,shuffle=True)
     validation_generator=DataGenerator(source_text=src_test,target_text=tgt_test,source_tokenizer=source_tokenizer,target_tokenizer=target_tokenizer,
                                      target_vocab_size=tgt_vsize,source_timesteps=SOURCE_TIMESTEPS,target_timesteps=TARGET_TIMESTEPS,batch_size=BATCH_SIZE,shuffle=True)
 
-    full_model=define_nmt(hidden_size=HIDDEN_SIZE,embedding_dim=EMBEDDING_DIM,
+    if os.path.isfile('h5.models/' + dir_hash + "/model.h5"):
+        print("Existing model found. loading")
+        full_model=load_model('h5.models/' + dir_hash + "/model.h5")
+    else:
+        print("No existing model found. Creating from scratch.")
+        full_model,encoder_model,decoder_model=define_nmt(hidden_size=HIDDEN_SIZE,embedding_dim=EMBEDDING_DIM,
                           source_lang_timesteps=SOURCE_TIMESTEPS,source_lang_vocab_size=src_vsize,
                           target_lang_timesteps=TARGET_TIMESTEPS,target_lang_vocab_size=tgt_vsize)
 
-    full_model.fit_generator(generator=training_generator,validation_data=validation_generator,use_multiprocessing=True,workers=6)
-    save_model(full_model,source_tokenizer,target_tokenizer)
+    full_model.fit_generator(generator=training_generator,validation_data=validation_generator,use_multiprocessing=True,workers=6,epochs=NUM_EPOCHS)
+    save_model(dir_hash,full_model,encoder_model,decoder_model,source_tokenizer,target_tokenizer)
