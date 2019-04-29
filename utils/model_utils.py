9acb83327f10c007d24aba2e7ff6cc0310874c1c
rohitapte <rohit@rohitapte.com>
2019-04-29 14:13:13 +0800

    moved model load and save to separate file

---
+++ utils/model_utils.py
@@ -0,0 +1,35 @@
+import os
+import io
+import json
+from tensorflow.python.keras.models import load_model
+from keras_preprocessing.text import tokenizer_from_json
+
+def save_model(dir_hash,model_dict,full_model,encoder_model,decoder_model,source_tokenizer,target_tokenizer):
+    if not os.path.exists('h5.models/' + dir_hash):
+        os.makedirs('h5.models/' + dir_hash)
+    with open('h5.models/' + dir_hash + "/model_params.json", 'w') as f:
+        f.write(json.dumps(model_dict))
+    tokenizer_json=source_tokenizer.to_json()
+    with io.open('h5.models/'+dir_hash+'/source_tokenizer.json','w',encoding='utf-8') as f:
+        f.write(json.dumps(tokenizer_json,ensure_ascii=False))
+    tokenizer_json=target_tokenizer.to_json()
+    with io.open('h5.models/'+dir_hash+'/target_tokenizer.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(tokenizer_json, ensure_ascii=False))
+    full_model.save('h5.models/'+dir_hash+'/full_model.h5')
+    encoder_model.save('h5.models/'+dir_hash+'/encoder_model.h5')
+    decoder_model.save('h5.models/'+dir_hash+'/decoder_model.h5')
+
+def load_saved_model(dir_hash):
+    with open('h5.models/'+dir_hash+'/model_params.json','r') as f:
+        for line in f:
+            data=json.loads(line)
+    with open('h5.models/'+dir_hash+'/source_tokenizer.json',encoding='utf-8') as f:
+        temp=json.load(f)
+        source_tokenizer=tokenizer_from_json(temp)
+    with open('h5.models/'+dir_hash+'/target_tokenizer.json',encoding='utf-8') as f:
+        temp=json.load(f)
+        target_tokenizer=tokenizer_from_json(temp)
+    full_model=load_model('h5.models/'+dir_hash+'/full_model.h5')
+    encoder_model=load_model('h5.models/'+dir_hash+'/encoder_model.h5')
+    decoder_model=load_model('h5.models/'+dir_hash+'/decoder_model.h5')
+    return data,source_tokenizer,target_tokenizer,full_model,encoder_model,decoder_model
