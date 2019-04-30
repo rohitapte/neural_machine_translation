f5f7b9e5ca5ca60b76b546583ac40640ec3011ee
rohitapte <rohit@rohitapte.com>
2019-04-30 08:54:51 +0800

    removed padding in embedding layer

--- models/encoder_decoder_gru.py
+++ models/encoder_decoder_gru.py
@@ -8,9 +8,9 @@ def define_nmt(hidden_size,embedding_dim,source_lang_timesteps,source_lang_vocab
     encoder_inputs=Input(shape=(source_lang_timesteps,),name='encoder_inputs')
     decoder_inputs=Input(shape=(target_lang_timesteps-1,),name='decoder_inputs')
 
-    encoder_embedding_layer = Embedding(input_dim=source_lang_vocab_size, output_dim=embedding_dim, mask_zero=True,input_length=source_lang_timesteps)
+    encoder_embedding_layer = Embedding(input_dim=source_lang_vocab_size, output_dim=embedding_dim)
     encoder_embedded = encoder_embedding_layer(encoder_inputs)
-    decoder_embedding_layer = Embedding(input_dim=target_lang_vocab_size, output_dim=embedding_dim, mask_zero=True,input_length=target_lang_timesteps)
+    decoder_embedding_layer = Embedding(input_dim=target_lang_vocab_size, output_dim=embedding_dim)
     decoder_embedded = decoder_embedding_layer(decoder_inputs)
 
     #encoder GRU
@@ -67,7 +67,7 @@ def translate(sentence,encoder_model,decoder_model,source_tokenizer,target_token
 
 if __name__ == '__main__':
     """ Checking nmt model for toy example """
-    full_model, encoder_model, decoder_model=define_nmt(hidden_size=64,embedding_dim=300, source_lang_timesteps=100,source_lang_vocab_size=254,target_lang_timesteps=100,target_lang_vocab_size=321,dropout=0.1)
+    full_model,encoder_model,decoder_model=define_nmt(hidden_size=64,embedding_dim=100,source_lang_timesteps=20,source_lang_vocab_size=254,target_lang_timesteps=20,target_lang_vocab_size=321,dropout=0.1)
     full_model.summary(line_length=225)
     encoder_model.summary(line_length=225)
     decoder_model.summary(line_length=225)
